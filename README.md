cd566b5c6f405e7f03d3453b77a0f22fb51f592e
Chris Smith <chris@pulumi.com>
2018-08-03 14:18:15 -0700

    Initial version of the app

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # browserhack-demo
+
 Demo of using Pulumi to deploy BrowserHack, a NetHack port for your browser.
+
+BrowserHack is the port of NetHack to JavaScript by @coolwanglu.
+The repo is here: https://github.com/coolwanglu/BrowserHack
+
+The relevant code in this repo is the Pulumi program that hosts it on a website,
+using Pulumi.
+
+## Standing Up
+
+```bash
+pulumi stack init browserhack-demo
+
+npm install
+npm run build
+npm run lint
+
+pulumi update
+```
---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as staticWebsite from "static-website-aws";
+
+const contentArgs: staticWebsite.ContentArgs = {
+    pathToContent: "./browserhack",
+    custom404Path: "/404.html",
+};
+
+const domainArgs: staticWebsite.DomainArgs = {
+    targetDomain: "browserhack.lawn-gnomes.net",
+    acmCertificateArn: "arn:aws:acm:us-east-1:153052954103:certificate/8d62a568-cc86-4e26-8f6e-08981e1bb7ca",
+};
+
+const website = new staticWebsite.StaticWebsite("browserhack", contentArgs, domainArgs);
+
+export let targetDomain = domainArgs.targetDomain;
+
