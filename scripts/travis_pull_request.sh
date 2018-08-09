80a55771b14cfd32fa44b49767ad4d54076e53de
Chris Smith <chris@pulumi.com>
2018-08-09 15:19:55 -0700

    Cleanup Travis build scripts

--- scripts/travis_pull_request.sh
+++ scripts/travis_pull_request.sh
@@ -1,10 +1,19 @@
-echo "Travis push job"
+echo "Travis pull_request job"
 
 set -e -x
 
+# Download dependencies and build
 npm install
 npm run build
 npm run lint
 
-pulumi stack select chrsmith/browserhack-demo
-PULUMI_PERSIST_PREVIEWS="1" pulumi preview
+# Preview changes that would be made if the PR were merged.
+case ${TRAVIS_BRANCH} in
+    master)
+        pulumi stack select chrsmith/browserhack-demo
+        pulumi preview
+        ;;
+    *)
+        echo "Pull Request to be merged into unknown branch. No stacks to preview."
+        ;;
+esac
--- scripts/travis_push.sh
+++ scripts/travis_push.sh
@@ -2,14 +2,16 @@ echo "Travis push job"
 
 set -e -x
 
+# Download dependencies and build
 npm install
 npm run build
 npm run lint
 
+# Update the stack
 case ${TRAVIS_BRANCH} in
     master)
         pulumi stack select chrsmith/browserhack-demo
-        pulumi update --yes
+        pulumi update
         ;;
     *)
         echo "Push to non-master branch. No stacks to update."
