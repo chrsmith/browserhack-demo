0cbc39dd5b4f066b083198a13db14e651c3f0f6b
Chris Smith <chrsmith@users.noreply.github.com>
2019-02-18 15:46:34 -0800

    Preview changes for PRs (#4)
    * Preview changes for PRs
    
    * Separate into different workflows

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+
+// staticWebsite is an NPM package that contains a Pulumi component for hosting
+// a static website on AWS.
+// https://github.com/chrsmith/static-website-aws
+// https://github.com/pulumi/examples/tree/master/aws-ts-static-website
 import * as staticWebsite from "static-website-aws";
 
 const contentArgs: staticWebsite.ContentArgs = {
@@ -5,11 +11,15 @@ const contentArgs: staticWebsite.ContentArgs = {
     custom404Path: "/404.html",
 };
 
+const config = new pulumi.Config();
+
 const domainArgs: staticWebsite.DomainArgs = {
-    targetDomain: "browserhack.lawn-gnomes.net",
-    acmCertificateArn: "arn:aws:acm:us-east-1:153052954103:certificate/8d62a568-cc86-4e26-8f6e-08981e1bb7ca",
+    targetDomain: config.require("targetDomain"),
+    acmCertificateArn: config.require("acmCertificate"),
 };
 
+// The StaticWebsite resource will create a Route53 DNS entry for the target domain,
+// copy the website's contents to an S3 bucket, and then serve it from CloudFront.
 const website = new staticWebsite.StaticWebsite("browserhack", contentArgs, domainArgs);
 
 export let targetDomain = domainArgs.targetDomain;
